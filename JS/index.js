b970faa19257b8ecd1c266df1f02d25654c97235
ManuReis2003 <manureis.application@gmail.com>
2024-09-05 10:42:35 -0300

    Adicionando modal

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,8 @@
 // Recebe a página toda
-const diaSemanaElement = document.getElementById("dia-semana")
-const diaMesAnoElement = document.getElementById("dia-mes-ano")
-const horaMinSegElement = document.getElementById("hora-min-seg")
-
+const diaSemana = document.getElementById("dia-semana")
+const diaMesAno = document.getElementById("dia-mes-ano")
+const horaMinSeg = document.getElementById("hora-min-seg")
+const arrayDayWeek =["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
 
 //diaSemana.textContent= "AAAAAAAA" // Elemento textual ( troca o valor do diaSemana )
 //console.log(diaSemana.textContent)
@@ -17,30 +17,44 @@ const horaMinSegElement = document.getElementById("hora-min-seg")
 // Formatação DATA/HORA Você precisa adicionar zeros à esquerda para os números menores que 10.
 
 // Função para retornar o dia da semana por extenso
-function getDiaSemana() {
-  const date = new Date();
-  const diasSemana =["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
-  return diasSemana[date.getDay()];
-}
+const dialogPonto = document.getElementById("dialog-ponto")
 
+const btnRegistrarPonto = document.getElementById("btn-registrar-ponto");
+btnRegistrarPonto.addEventListener("click", () => {
+    dialogPonto.showModal();
+});
+
+
+const btnDialogFechar = document.getElementById("btn-dialog-fechar");
+btnDialogFechar.addEventListener("click", () => {
+    dialogPonto.close();
+});
+
+// 
+function daySemana() {
+    //retorna dia semana
+  const date = new Date();
+  return arrayDayWeek[date.getDay()];
+}
 // Função para retornar a data completa
 function dataCompleta(){ 
   const date = new Date();     //FUNÇÃO QUE TRAZ A DATA 
   return date.getDate()+ "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
 }
-
-
-// Função para retornar a hora completa
                 // PAD padStart(2, "0"): Adiciona zeros à esquerda da string até que ela tenha pelo menos 2 caracteres. Isso garante que a hora seja sempre exibida com 2 dígitos 
-function horaCompleta() {
+function horaCompleta() {  // FUNÇÃO QUE TRAZ A HORA COMPLETA
   const date = new Date();
-  const horas = date.getHours().toString().padStart(2, "0");   //date.getHours(): Obtém a hora atual como um número (0-23). toString(): Converte o número em uma string.
-  const minutos = date.getMinutes().toString().padStart(2, "0");   //Essa linha obtém os minutos atuais a partir do objeto Date e os converte em uma string
-  const segundos = date.getSeconds().toString().padStart(2, "0");
-  return `${horas}:${minutos}:${segundos}`;   //Essa linha retorna uma string que representa a hora completa no formato "HH:MM:SS". 
+  return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    //date.getHours(): Obtém a hora atual como um número (0-23). toString(): Converte o número em uma string.
+     //Essa linha obtém os minutos atuais a partir do objeto Date e os converte em uma string
+     //Essa linha retorna uma string que representa a hora completa no formato "HH:MM:SS". 
 }
-
+function atualizaHora(){
+    horaMinSeg.textContent = horaCompleta();                                             //-> tempo de intervalo entre as execuções em horas
+                                //funçaõ que quero executar 
+}
+setInterval(atualizaHora,1000);
 // Atualiza os elementos da página       /sobreescreve oq ta no HTML com oq a função retorna
-diaMesAnoElement.textContent = getDataCompleta();
-horaMinSegElement.textContent = getHoraCompleta();
-diaSemanaElement.textContent = getDiaSemana();
+diaMesAno.textContent = dataCompleta();
+horaMinSeg.textContent = horaCompleta();
+diaSemana.textContent = daySemana();
