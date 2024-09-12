cac35c3736349debe5ac68e07efd2dc353d62709
ManuReis2003 <manureis.application@gmail.com>
2024-09-12 10:14:37 -0300

    Adicionando o modal

--- JS/index.js
+++ JS/index.js
@@ -19,6 +19,20 @@ const arrayDayWeek =["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira",
 // Função para retornar o dia da semana por extenso
 const dialogPonto = document.getElementById("dialog-ponto")
 
+navigator.geolocation.getCurrentPosition ((position) => {  // Puxa API que retorna loc atua
+  console.log(position);
+  console.log(position);
+  console.log(position)
+});
+
+//To do : 
+
+dialog tipoPonto = document.getElementById ("select-tipos-")
+//Apresentar para o user a data e hora atualizados
+// atualizar a data todos os dia 0:00
+//atualizar a hr td segundo
+//Organizar código
+
 const btnRegistrarPonto = document.getElementById("btn-registrar-ponto");
 btnRegistrarPonto.addEventListener("click", () => {
     dialogPonto.showModal();
@@ -30,6 +44,29 @@ btnDialogFechar.addEventListener("click", () => {
     dialogPonto.close();
 });
 
+
+const btnDialogRegistrarPonto = documen.getElementById("btndialog-registrar-ponto");
+btnDialogRegistrarPonto.addEventListener("Click", () => { 
+
+  let dataComleta = dataCompleta();
+  let hora = horacompleta();
+  let
+  let ponto = { 
+    "data": dataCompleta
+  }
+
+
+  const tipoPonto = document.getElementById("select-tipos-ponto");
+  btnDialogRegistrarPonto.addEventListener("click", ()=> { 
+
+    let ponto = {
+
+    }
+
+  })
+});
+// Todo conjunto numérico (exceto ano) deve ter 2 dígitos (adicionar 0 se for menor q 10)
+// Retornar dia da semana por extenso (em pt-br)
 // 
 function daySemana() {
     //retorna dia semana
@@ -38,13 +75,13 @@ function daySemana() {
 }
 // Função para retornar a data completa
 function dataCompleta(){ 
-  const date = new Date();     //FUNÇÃO QUE TRAZ A DATA 
-  return date.getDate()+ "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+  const date = new Date();     //FUNÇÃO QUE TRAZ A DATA   -- padstart - define tamanho da string (dígitos)
+  return date.getDate().padsart(2,'0') + "/" + String(date.getMonth() + 1).padStart(2,'0') + "/" + date.getFullYear();
 }
                 // PAD padStart(2, "0"): Adiciona zeros à esquerda da string até que ela tenha pelo menos 2 caracteres. Isso garante que a hora seja sempre exibida com 2 dígitos 
 function horaCompleta() {  // FUNÇÃO QUE TRAZ A HORA COMPLETA
   const date = new Date();
-  return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  return String( date.getHours()).padStart(2,'0') + ":" + String(date.getMinutes()).padStart(2,'0') + ":" + date.getSeconds();
     //date.getHours(): Obtém a hora atual como um número (0-23). toString(): Converte o número em uma string.
      //Essa linha obtém os minutos atuais a partir do objeto Date e os converte em uma string
      //Essa linha retorna uma string que representa a hora completa no formato "HH:MM:SS". 
