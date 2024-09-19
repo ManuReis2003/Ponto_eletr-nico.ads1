f212828c0574da7448af374f33c0bdf65bcd11b3
ManuReis2003 <manureis.application@gmail.com>
2024-09-19 10:29:56 -0300

    Adicionando localStorage

--- JS/index.js
+++ JS/index.js
@@ -19,22 +19,50 @@ const arrayDayWeek =["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira",
 // Função para retornar o dia da semana por extenso
 const dialogPonto = document.getElementById("dialog-ponto")
 
-navigator.geolocation.getCurrentPosition ((position) => {  // Puxa API que retorna loc atua
+navigator.geolocation.getCurrentPosition((position) => {   // API 
   console.log(position);
-  console.log(position);
-  console.log(position)
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
 });
-
 //To do : 
 
-dialog tipoPonto = document.getElementById ("select-tipos-")
 //Apresentar para o user a data e hora atualizados
 // atualizar a data todos os dia 0:00
 //atualizar a hr td segundo
 //Organizar código
+//salvar o histório de ponto ( não sobrescrever)
+//salvar no localstore o último tipo de pono do usuário
+//Recuperar localização do user
+// Ao registrar ponto, fechar modal
+// Estilo 
+
+
+//3- Fazer uma condicional e atrbuir o valor o select confroe a tabela:
+   // tipo útimo ponto  | valor select 
+   // entrada           | intervalo
+   //intervalo          | volta-intervalo
+   //volta-intervalo    | saída
+   //saída              | entrada
+    // TO-DO:
+    // 1 - recuperar o select por meio de id ("select-tipos-ponto")
+    let dialogSelect = document.getElementById("select-tipos-ponto");
+    
+    // 2 - recuperar o tipo do ultimo ponto que está salvo no localstorage
+    // 2.1 - salvamos o tipo na chave "tipoUltimoPonto"
+    // 2.2 - conseguimos recuperar um valor do localstorage com o getItem(chave)
+    let ultimoPonto = localStorage.getItem("tipoUltimoPonto");
+    
+    // 3 - Fazer uma condicional e atribuir o valor do select conforme tabela
+    // tipo ultimo ponto  |  valor select
+    // entrada            |  intervalo
+    // intervalo          |  volta-intervalo
+    // volta-intervalo    |  saida
+    // saida              |  entrada
+    // let proximoPonto = proxPonto[/*tipo do último ponto*/];
+    // 3.1 para setar o valor de um select, usamos o atributo value
 
 const btnRegistrarPonto = document.getElementById("btn-registrar-ponto");
-btnRegistrarPonto.addEventListener("click", () => {
+btnRegistrarPonto.addEventListener("click", () => { // () => ARROW FUNCTION)
     dialogPonto.showModal();
 });
 
@@ -45,29 +73,32 @@ btnDialogFechar.addEventListener("click", () => {
 });
 
 
-const btnDialogRegistrarPonto = documen.getElementById("btndialog-registrar-ponto");
+const btnDialogRegistrarPonto = document.getElementById("btn-dialog-registrar-ponto");
 btnDialogRegistrarPonto.addEventListener("Click", () => { 
 
-  let dataComleta = dataCompleta();
-  let hora = horacompleta();
-  let
-  let ponto = { 
-    "data": dataCompleta
+  let dataCompleta = dataCompleta();
+  let hora = horaCompleta();
+  let tipoPonto = document.getElementById("select-tipos-ponto").value;
+
+
+  let ponto = {
+    "data": dataCompleta,
+    "hora": hora,
+    "tipo": tipoPonto,
+    "id": 1
   }
+  console.log(ponto);     ////////////////////////
+  
+ localStorage.setItem("registro", JSON.String(ponto));      //armazena dados
+ localStorage.setItem("tipoUltimoponto", tipoPonto);
+ });
 
-
-  const tipoPonto = document.getElementById("select-tipos-ponto");
-  btnDialogRegistrarPonto.addEventListener("click", ()=> { 
-
-    let ponto = {
-
-    }
-
-  })
-});
 // Todo conjunto numérico (exceto ano) deve ter 2 dígitos (adicionar 0 se for menor q 10)
 // Retornar dia da semana por extenso (em pt-br)
-// 
+// recuperar tipo do último acesso 
+// salvamos o tipo de chave 
+
+
 function daySemana() {
     //retorna dia semana
   const date = new Date();
@@ -76,7 +107,7 @@ function daySemana() {
 // Função para retornar a data completa
 function dataCompleta(){ 
   const date = new Date();     //FUNÇÃO QUE TRAZ A DATA   -- padstart - define tamanho da string (dígitos)
-  return date.getDate().padsart(2,'0') + "/" + String(date.getMonth() + 1).padStart(2,'0') + "/" + date.getFullYear();
+  return String(date.getDate()).padtart(2,'0') + "/" + String(date.getMonth() + 1).padStart(2,'0') + "/" + date.getFullYear();
 }
                 // PAD padStart(2, "0"): Adiciona zeros à esquerda da string até que ela tenha pelo menos 2 caracteres. Isso garante que a hora seja sempre exibida com 2 dígitos 
 function horaCompleta() {  // FUNÇÃO QUE TRAZ A HORA COMPLETA
